e2bc395127597f2e1e68a871a596b58ce2e5bb2f
yahya <yahya1haloui@gmail.com>
2024-10-18 20:11:55 -0400

    Ajout des exclusions dans .gitignore pour les fichiers générés par Visual Studio

---
+++ AppConsoleLivres/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AppConsoleLivres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenue dans l'application AppConsoleLivres !");
+            // Ajoutez ici votre logique
+        }
+    }
+}
