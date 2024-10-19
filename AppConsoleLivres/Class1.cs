542598dcc000f3140d7350d360d90ba6f2a51d06
yahya <yahya1haloui@gmail.com>
2024-10-18 20:13:42 -0400

    Ajout la classe mere Livre

---
+++ AppConsoleLivres/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace AppConsoleLivres
+{
+    public class Livre
+    {
+        
+        public string Titre { get; set; }
+        public string Auteur { get; set; }
+        public int AnneePublication { get; set; }
+        public string ISBN { get; set; }
+
+        
+        public Livre(string titre, string auteur, int anneePublication, string isbn)
+        {
+            Titre = titre;
+            Auteur = auteur;
+            AnneePublication = anneePublication;
+            ISBN = isbn;
+        }
+
+        
+        public void AfficherInfo()
+        {
+            Console.WriteLine($"Titre : {Titre}, Auteur : {Auteur}, Année : {AnneePublication}, ISBN : {ISBN}");
+        }
+    }
+}
